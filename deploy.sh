0d6bea19f5b81416a8a3bf22900d2ee19597ebcf
chengjun <wangchj04@gmail.com>
2020-03-28 18:43:59 +0800

    rebuilding site

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+#git config 'lfs.https://github.com/chengjun/bigdata.git/info/lfs.locksverify' false
+
+# git pull first
+# git pull origin master
+# ssh-add ~/.ssh/id_rsa
+# git remote set-url origin git@github.com:chengjun/bigdata.git
+#
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
